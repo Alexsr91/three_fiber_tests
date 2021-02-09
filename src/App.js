dc92aff8346da81f32c21d2df15751e4bdff2b08
Alex <helloalexsr@gmail.com>
2021-02-09 21:44:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber"
+import { OrbitControls, Stars } from "@react-three/drei";
+
+import Joder from "./Joder.js";
+// import Jupiter from "./Jupiter.js"
+
 import './App.css';
 
-function App() {
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <Canvas>
+    <OrbitControls />
+      <ambientLight intensity={0.6} />
+      <directionalLight intensity={0.5} />
+      <spotLight position={[0, 1, 1]} angle={0.3} />
+      
+      <Suspense fallback={null}>
+        <Stars count={1000} />
+        <Joder />
+        {/* <Jupiter/> */}
+      </Suspense>
+  </Canvas>
   );
 }
 
-export default App;
+
+
